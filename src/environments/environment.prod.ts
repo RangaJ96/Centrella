93022463c96308977899bda82601679bde816651
RangaJ96 <49330699+RangaJ96@users.noreply.github.com>
2020-05-04 10:40:37 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   firebase: {
     
-    apiKey: "AIzaSyBgUjAYw679wOKxbEjC0wZ3G3JpCNpNst0",
-    authDomain: "chattest-96ba0.firebaseapp.com",
-    databaseURL: "https://chattest-96ba0.firebaseio.com",
-    projectId: "chattest-96ba0",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "762742814171",
-    appId: "1:762742814171:web:3c6dea43c7b6721a"
+    messagingSenderId: "",
+    appId: ""
     
   },
   
